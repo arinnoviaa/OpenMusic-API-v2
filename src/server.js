231ca4df27e63b090349cc7fbffcd6cb433bcf02
Arin Novia <arinnoviaaa@gmail.com>
2022-08-17 00:42:35 +0700

    authentication plugin pada hapi server

--- src/server.js
+++ src/server.js
@@ -18,10 +18,18 @@ const users = require('./api/users');
 const UsersService = require('./services/postgres/usersService');
 const UsersValidator = require('./validator/users');
 
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/authenticationsService');
+const TokenManager = require('./tokenize/tokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
   const albumsService = new AlbumsService();
   const songsService = new SongsService();
   const usersService = new UsersService();
+  const authenticationsService = new AuthenticationsService();
+
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -52,6 +60,15 @@ const init = async () => {
       options: {
         service: usersService,
         validator: UsersValidator,
+      },
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
       },
     },
   ]);
